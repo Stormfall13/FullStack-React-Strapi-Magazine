2f0b489bd06f3c67dcb9b8d9beb0cacfe8ea246c
Storm <stormfall@gmail.com>
2023-04-29 15:46:08 +0500

    first commit

---
+++ client/src/scenes/checkout/Confirmation.jsx
@@ -0,0 +1,19 @@
+import { Box, Alert, AlertTitle } from '@mui/material';
+
+const Confirmation = () => {
+    return (
+    <Box
+        m="90px auto"
+        width="80%"
+        height="50vh"
+    >
+        <Alert severity="success">
+            <AlertTitle>Success</AlertTitle>
+            You have successfull made an Order - {" "}
+            <strong>Congrats on maklng your purchase</strong>
+        </Alert>
+    </Box>
+    );
+};
+
+export default Confirmation;
